cc219a25ce5ab0fa266d5f25a3a7a7ac20612a68
noruzzamanrubel <noruzzamanrubel@gmail.com>
2024-02-07 08:20:12 +0600

    update v 1.4.2

--- includes/class-autocomplete-woocommerce-order-status.php
+++ includes/class-autocomplete-woocommerce-order-status.php
@@ -70,7 +70,7 @@ class Autocomplete_Woocommerce_Order_Status {
 		if ( defined( 'AUTOCOMPLETE_WOOCOMMERCE_ORDER_STATUS_VERSION' ) ) {
 			$this->version = AUTOCOMPLETE_WOOCOMMERCE_ORDER_STATUS_VERSION;
 		} else {
-			$this->version = '1.4.1';
+			$this->version = '1.4.2';
 		}
 		$this->plugin_name = 'autocomplete-woocommerce-order-status';
 
